cda42723ba283168b55175eeb71a14b33990067f
matt4110 <matthew.johnson4110@gmail.com>
2016-06-14 09:28:20 -0400

    FAQ page styling

--- wp-content/themes/war-theme/sidebar-FAQ.php
+++ wp-content/themes/war-theme/sidebar-FAQ.php
@@ -8,27 +8,73 @@
  */
 ?>
 
-		<div id="primary" class="widget-area" role="complementary">
-			<ul class="xoxo">
+	<div id="tertiary" class="widget-area" role="complementary">
+		<ol id="faq-list">
+			<h1>List of Questions & Links to the Answers</h1><br>
+			<strong>Questions about our Programs & Partners</strong>
+			<li><a href="#q1">How many countries does Women At Risk, International (WAR, Int’l) work in? </a></li>
+			<li><a href="#q2">Is trafficking the only risk factor that WAR, Int’l works with? How do you define an at-risk woman?</a></li> 
+			<li><a href="#q3">Does WAR, Int’l help only women and girls? Do any of WAR, Intl’s partners work with at-risk men or boys?</a></li>
+			<li><a href="#q4">Does WAR, Int’l partner only with safehouses? What other kinds of organizations does WAR, Int’l support?</a></li>
+			<li><a href="#q5">How are relationships with WAR, Int’l’s partners developed? What criteria does WAR, Int’l use in deciding what organizations to partner with?</a></li>
+			<li><a href="#q6">How do WAR, Int’l partnerships work? How does WAR, Int’l work with its partnering organizations?</a></li>
+			<li><a href="#q7">How many women and children does WAR, Int’l help each year in partnering safehouses and other programs?</a></li> 
+			<li><a href="#q8">Why doesn’t WAR, Int’l list the names of its partners? </a></li>
 
-<?php
-	/* When we call the dynamic_sidebar() function, it'll spit out
-	 * the widgets for that widget area. If it instead returns false,
-	 * then the sidebar simply doesn't exist, so we'll hard-code in
-	 * some default sidebar stuff just in case.
-	 */
-	if ( ! dynamic_sidebar( 'primary-widget-area' ) ) : ?>
+			<strong>Questions About Trafficking and Rescue</strong>
 
-			<li id="meta" class="widget-container">
-				<h3 class="widget-title"><?php _e( 'Meta', 'twentyten' ); ?></h3>
-				<ul>
-					<?php wp_register(); ?>
-					<li><?php wp_loginout(); ?></li>
-					<?php wp_meta(); ?>
-				</ul>
-			</li>
+			<li><a href="#q9">What exactly is “human trafficking”?</a></li>
+			<li><a href="#q10">Globally, how many people are trafficked each year for sex or labor purposes?</a></li>
+			<li><a href="#q11">Does trafficking happen in the United States? Are children trafficked in the U.S.?</a></li>
+			<li><a href="#q12">What is the government doing about trafficking in the U.S.? Are there other organizations working to stop trafficking in the United States?</a></li>
+			<li><a href="#q13">Why would traffickers be interested in babies? Are rescued babies able to be adopted?</a></li>
+			<li><a href="#q14">How does WAR, Int’l rescue women and children?</a></li>
+			<li><a href="#q15">Is there danger to partners and staff when they rescue women?</a></li>
+			<li><a href="#q16">When women are rescued, do they go straight to a safehouse?</a></li>
+			<li><a href="#q17">What exactly is a safehouse? How can a safehouse be non-residential? Are the safehouses really safe? </a></li>
+			<li><a href="#q18">Does WAR, Int’l partner with any safehouses in the United States? </a></li>
+			<li><a href="#q19">Do rescued women ever return to the streets?</a></li>
+			<li><a href="#q20">Does WAR, Int’l pursue the prosecution of the trafficker?</a></li>
+			<li><a href="#q21">Is rescue enough?</a></li>
+			<li><a href="#q22">Why is WAR, Int’l so passionate about job training? What kinds of job training do WAR, Int’l’s partners offer? How do you decide what job training works? </a></li>
+			<li><a href="#q23">Do rescued individuals work here in the United States?</a></li>
+			<li><a href="#q24">How does WAR, Int’l return rescued women to their homes?</a></li>
+			<li><a href="#q25">How does WAR, Int’l work with at-risk women in Islamic countries?</a></li>
+			<li><a href="#q26">When it is difficult or impossible to safely remove the victim from the situation, how does WAR, Int’l work with the abuser?</a></li>
 
-		<?php endif; // end primary widget area ?>
-			</ul>
-		</div><!-- #primary .widget-area -->
+			<strong>Questions About Our Products</strong>
+
+			<li><a href="#q27">Where do the materials used in Women At Risk, International’s (WAR, Int’l’s) products come from?</a></li>
+			<li><a href="#q28">How much of the money from product sales goes into programs? How much goes back to the women who make the products? How are the women paid?</a></li>
+			<li><a href="#q29">Do all of WAR, Int’l’s partners make and sell product?</a></li>
+			<li><a href="#q30">What kinds of products does WAR, Int’l sell? Do you have a catalog? How can I purchase your products?</a></li>
+			<li><a href="#q31">Are all of WAR, Int’l’s products made by rescued women? </a></li>
+
+			<strong>Questions About Getting Involved</strong>
+
+			<li><a href="#q32">Do you send out newsletters? Can I receive them via email? </a></li>
+			<li><a href="#q33">How can I get involved?</a></li>
+			<li><a href="#q34">How can I use my home, my family, and my circle of influence here to raise awareness about human trafficking and help WAR, Int’l create circles of protection and hope around at-risk women? </a></li>
+			<li><a href="#q35">How do I host a WAR party? Will it cost me anything to host a party?</a></li>
+			<li><a href="#q36">How can I use my business or my professional skills to assist WAR, Int’l in the fight against trafficking?</a></li>
+			<li><a href="#q37">How can I have Becky come and speak at my event? Does WAR, Int’l send other speakers to parties, conferences, or church and organizational presentations? </a></li>
+			<li><a href="#q38">Are internships available with WAR, Int’l? Can I work in the U.S. or overseas? Can I serve in a safehouse or orphanage as a short-term missionary?</a></li>
+			<li><a href="#q39">How can I give to WAR, Int’l?</a></li>
+			<li><a href="#q40">What are Circle Tours? How often do they happen? Are Circle Tours safe? Who can go on a Circle Tour? How do I sign up?</a></li>
+			<li><a href="#q41">Is my story of wounding and restoration of any help to WAR, Int’l?</a></li>
+
+			<strong>Questions About the Organization</strong>
+
+			<li><a href="#q42">What is the passion behind the founding of Women At Risk, International (WAR, Int’l)?</a></li>
+			<li><a href="#q43">When and how did WAR, Int’l start?</a></li>
+			<li><a href="#q44">Has WAR, Int’l staff witnessed the problem first-hand? Have staff members visited WAR, Int’l’s partners?</a></li>
+			<li><a href="#q45">Is WAR, Int’l a “religious” organization?</a></li>
+			<li><a href="#q46">How are monies raised and delegated within WAR, Int’l? To whom is WAR, Int’l financially accountable? </a></li>
+			<li><a href="#q47">How is WAR, Int’l organized? </a></li>
+			<li><a href="#q48">How many staff does WAR, Int’l have working stateside and overseas? </a></li>
+			<li><a href="#q49">Where is WAR, Int’l located? How can I contact you?</a></li>
+
+
+		</ol>
+	</div>
 
