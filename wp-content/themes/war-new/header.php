d67124e21fbf5594b16cd4308e09aae6315ed901
matt4110 <matthew.johnson4110@gmail.com>
2016-07-06 23:08:56 -0400

    building main nav menu

--- wp-content/themes/war-new/header.php
+++ wp-content/themes/war-new/header.php
@@ -25,16 +25,52 @@
 <div id="page" class="site">
 	<div class="site-inner">
 
-		<header id="header" role="banner">
-			<a href=<?php get_bloginfo( 'url' ); ?> ><img src="" alt="women at risk logo"></a>
-			<div>
-				<ul class="horizontal-list-right">
+		<header role="banner">
+			<div id="header">
+				<a href=<?php get_bloginfo( 'url' ); ?> ><img src="" alt="women at risk seal"></a>
+				<img src="" alt="women at risk logo">
+				<ul class="horizontal-list-right no-margin">
 					<li class="horizontal-li--gold-border"><a href="http://store.warchestboutique.com">SHOP WITH A PURPOSE</a></li>
 					<li class="horizontal-li--gold-border"><a href=<?php get_bloginfo( 'url' ); ?>"/get-involved/host-a-party">HOST A PARTY</a></li>
 					<li class="horizontal-li--gold-border"><a href=<?php get_bloginfo( 'url' ); ?>"/events">UPCOMING EVENTS</a></li>
 				</ul>
+				<div class="clearfix"></div>
 			</div>
-			<div class="clearfix"></div>
+			<ul class="full-width-nav--black">
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?> >Home</a></li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">About Us</a>
+					<ul class="sub-nav">
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Location & Hours</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Contact Us</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"">FAQ</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Endorsements</a></li>
+					</ul>
+				</li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">News & Media</a>
+					<ul class="sub-nav">
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Upcoming Events</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Story Gallery</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Sweetie Updates</a></li>
+					</ul>
+				</li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Get Involved</a>
+					<ul class="sub-nav">
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Understand the Risks</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Upcoming Events</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Host A Party</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Civilian First Responders</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Fundraisers</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">S.O.A.P. Project</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Team WAR</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">One Light Defense</a></li>
+						<li class="sub-nav-li"><a href=<?php get_bloginfo( 'url' ); ?>"/">Liz Curtis Higgs Event</a></li>
+					</ul>
+				</li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Programs</a></li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Donate</a></li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Store</a></li>
+				<li class="horizontal-li"><a href=<?php get_bloginfo( 'url' ); ?>"">Prayer</a></li>
+			</ul>
 		</header><!-- .site-header -->
 
 		<div id="content" class="site-content">
--- wp-content/themes/war-new/readme.txt
+++ wp-content/themes/war-new/readme.txt
@@ -1,13 +1,11 @@
 A few notes about this theme:
 
 build started on May 31, 2016 by Matthew Johnson (Women At Risk, international webmaster)
-
 This is a child theme of WordPress twenty-sixteen theme
 
-This theme utilizes Sass for its CSS preprocessor, so do not write to style.css or they will be overwritten the next time Sass is ran. 
+Previously, the sites backend grew to a chaotic extent. To battle this and to be able to allow others into to backend without fear of messing much up as much of this site that could be while still keeping the site agile and flexible has been hard coded into the files. This also includes building our own javascript functionality instead of relying on plugins. This is in hopes of keeping the site easy to maintain, flexible, and protected from inexperienced users.
 
-The organization of the stylesheet follows the ideas in CSS for Growns Ups presented at 2012 SXSW Interactive. Check it out here: https://www.youtube.com/watch?v=ZpFdyfs03Ug This is a very similar to SMACSS and OOCSS. If you are unfamiliar with them a quick read: https://www.sitepoint.com/css-architectures-scalable-and-modular-approaches/
-
-Also, the bourbon and neat Sass libraries have been included. These are great light weight libraries that enable a speedy, consistent development.
+This theme utilizes Sass for its CSS preprocessor, so do not write to style.css or they will be overwritten the next time Sass is ran. The organization of the stylesheet follows the ideas in CSS for Growns Ups presented at 2012 SXSW Interactive. Check it out here: https://www.youtube.com/watch?v=ZpFdyfs03Ug This is a very similar to SMACSS and OOCSS. If you are unfamiliar with them a quick read: https://www.sitepoint.com/css-architectures-scalable-and-modular-approaches/
+Also, the bourbon Sass library has been included. This is a great light weight library that reduces repetitive processes in the CSS.
 
 This site is also available on GitHub, but it is a private directory. If you would like access to this repository please email me at Matthew.Johnson4110@gmail.com
