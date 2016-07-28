f60033ebf4847f39e21bdd0a30689f8d5dcbb0f3
matt4110 <matthew.johnson4110@gmail.com>
2016-07-28 11:27:26 -0400

    Footer built

--- wp-content/themes/war-2016/footer.php
+++ wp-content/themes/war-2016/footer.php
@@ -13,13 +13,26 @@
 		</div><!-- .site-content -->
 
 		<footer id="footer">
-			<div id="footer-left">
-				<a href=<?php get_bloginfo( 'url' ); ?>><img id="footer-logo" src="/wp-content/themes/war-2016/assets/images/war-logo.png"></a>
-				<p>(616)855-0796</p>
-				<p><a href="mailto:info@warinternational.org">info@warinternational.org</a></p>
+			<div class="row">
+				<div class="footer-left">
+					<a href=<?php get_bloginfo( 'url' ); ?>><img id="footer-logo" src="/wp-content/themes/war-2016/assets/images/war-logo.png"></a>
+					<p class="info">(616)855-0796</p>
+					<p class="info"><a href="mailto:info@warinternational.org">info@warinternational.org</a></p>
+				</div>
+				<div class="footer-right">
+					<a href="http://instagram.com/the_war_chest_boutique/" target="_blank"><img class="footer-icon" src="/wp-content/themes/war-2016/assets/images/icon-instagram.png"></a>
+					<a href="http://pinterest.com" target="_blank"><img class="footer-icon" src="/wp-content/themes/war-2016/assets/images/icon-pinterest.png"></a>
+					<a href="http://youtube.com/womenatrisk" target="_blank"><img class="footer-icon" src="/wp-content/themes/war-2016/assets/images/icon-youtube.png"></a>
+					<a href="http://twitter.com/WARpresident/" target="_blank"><img class="footer-icon" src="/wp-content/themes/war-2016/assets/images/icon-twitter.png"></a>
+					<a href="http://facebook.com/womenatrisk/" target="_blank"><img class="footer-icon" src="/wp-content/themes/war-2016/assets/images/icon-facebook.png"></a>
+				</div>
+				<div class="clearfix"></div>
 			</div>
-			<div id="footer-right">
+			<div class="footer-left" id="ecfa">
+				<img id="ecfa-icon" src="/wp-content/themes/war-2016/assets/images/ECFA.png">
+				<p id="ecfa-text">WAR, Int'l is certified with the ECFA, an accreditation agency dedicated to both providing financial accountability and upholding financial integrity. <a href="http://www.ecfa.org">www.ecfa.org</a></p>
 			</div>
+			<div class="clearfix"></div>
 
 			
 
