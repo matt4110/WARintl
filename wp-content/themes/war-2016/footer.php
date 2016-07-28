f7408c4d60925df666e34ad16850435bde3f2825
matt4110 <matthew.johnson4110@gmail.com>
2016-07-28 08:55:34 -0400

    renamed war-new to war-2016

---
+++ wp-content/themes/war-2016/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+?>
+
+		</div><!-- .site-content -->
+
+		<footer id="footer">
+			<img id="footer-logo" src="/wp-content/themes/war-new/assets/images/war-logo.png">
+
+			
+
+		</footer><!-- .site-footer -->
+	</div><!-- .site-inner -->
+</div><!-- .site -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
