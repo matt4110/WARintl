ed21f5c3bfe4289aeaf8c7135f36ada1b7ac2854
matt4110 <matthew.johnson4110@gmail.com>
2016-08-03 21:28:42 -0400

    built slider

---
+++ wp-content/themes/war-2016/page-home.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ Template Name: Home Page
+
+ */
+
+get_header(); ?>
+	<div id="slider">
+		<figure id="slides-holder">
+			<img class="slides" src="/wp-content/themes/war-2016/assets/images/slider/slide1.jpg">
+			<img class="slides" src="/wp-content/themes/war-2016/assets/images/slider/slide2.jpg">
+			<img class="slides" src="/wp-content/themes/war-2016/assets/images/slider/slide3.jpg">
+			<img class="slides" src="/wp-content/themes/war-2016/assets/images/slider/slide4.jpg">
+		</figure>
+	</div>
+	<div id="primary" class="content-area">
+		
+
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
