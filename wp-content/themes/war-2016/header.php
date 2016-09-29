5b4011b1f4370729266d21ca8bf560c56452a700
matt4110 <matthew.johnson4110@gmail.com>
2016-09-29 09:14:49 -0400

    box menu

--- wp-content/themes/war-2016/header.php
+++ wp-content/themes/war-2016/header.php
@@ -29,6 +29,9 @@
 			<div id="header">
 				<a href=<?php get_bloginfo( 'url' ); ?> ><img id="seal" src="/wp-content/themes/war-2016/assets/images/war-seal.png" alt="women at risk seal"></a>
 				<img id="logo" src="/wp-content/themes/war-2016/assets/images/war-logo.png" alt="women at risk logo">
+				<div id="donate">
+					<h1 id="donate-header">DONATE</h1>
+				</div>
 				<ul class="horizontal-list-right no-margin">
 					<li class="horizontal-li--gold-border"><a href="http://store.warchestboutique.com">SHOP WITH A PURPOSE</a></li>
 					<li class="horizontal-li--gold-border"><a href=<?php get_bloginfo( 'url' ); ?>"/get-involved/host-a-party">HOST A PARTY</a></li>
--- wp-content/themes/war-2016/page-home.php
+++ wp-content/themes/war-2016/page-home.php
@@ -6,6 +6,14 @@
 
 get_header(); ?>
 
+	<div class="filigree no-margin">
+	&nbsp;
+	</div>
+	<div id="mission-box" class="no-margin">
+		<h1>Our Mission</h1>
+		<p>Women At Risk International unites and educates women to create circles of protection and hope around women at risk through culturally sensitive, value-added intervention projects.</p>
+	</div>
+
 	<div class="carousel-wrapper">
 		<div class="carousel">
 		    <div class="inner">
@@ -27,28 +35,78 @@ get_header(); ?>
 	<!-- Box Menu -->
 	<div id="primary" class="content-area">
 		<div class="box-menu">
-			<div class="box-menu-item">
-				<div class="box-menu-image">
-					<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-store.jpg">
+
+		<div class="row">
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-store.jpg">
+					</div>
+					<div class="box-menu-split-text">
+						<h1>shop</h1>
+						<h1 class="small">with a</h1>
+						<h1>purpose</h1>
+						<p>Gifts of International Distinction</p>
+					</div>
+
 				</div>
 
-				<div class="box-menu-split-text">
-					<h1>shop</h1>
-					<h1 class="small">with a</h1>
-					<h1>purpose</h1>
-					<p>Gifts of International Distinction</p>
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-ustc.jpg">
+					</div>
+					<div class="box-menu-split-text">
+						<h1>U.S.<br>Training<br>Center</h1>
+						<p>Healing Hearts in the Homeland</p>
+					</div>
 				</div>
-
 			</div>
 
-			<div class="box-menu-item">
-				<div class="box-menu-image">
-					<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-ustc.jpg">
+			<div class="row">
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-party.jpg">
+					</div>
+					<div class="box-menu-split-text">
+						<h1>Host A<br>Party</h1>
+						<img style="max-width: 100%; margin-bottom:0;" src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/icon-host-a-party.png">
+						<p style="text-align: center;">SAVE A LIFE</p>
+					</div>
 				</div>
-				<div class="box-menu-split-text">
-					<h1>Shop with a Purpose</h1>
+
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-get-involved.jpg">
+					</div>
+					<div style="padding:2%;" class="box-menu-split-text">
+						<h1>Get<br>Involved</h1>
+						<p>Donate your time, talent, or treasure to help rescue, redeem, & empower</p>
+					</div>
 				</div>
 			</div>
+
+			<div class="row">
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-cfr.jpg">
+					</div>
+					<div style="padding:2%;" class="box-menu-split-text">
+						<h1>Civilian<br>First</h1>
+						<h1 class="medium">Responder</h1>
+						<p>Learn to recognize the signs of sex trafficking within your scope of influence</p>
+					</div>
+				</div>
+
+				<div class="box-menu-item">
+					<div class="box-menu-image">
+						<img src=<?php get_bloginfo('url'); ?>"/wp-content/themes/war-2016/assets/images/box-stories.jpg">
+					</div>
+					<div class="box-menu-split-text">
+						<h1>Stories<br>of hope</h1>
+						<p>Learn how your purchase lifts lifes to dignity</p>
+					</div>
+				</div>
+			</div>
+
 		<!-- /Box Menu -->	
 		</div>
 		<div class="clearfix"></div>
