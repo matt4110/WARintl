c83cd4cfaf5d1ee2d758496def808bdedff98db4
matt4110 <matthew.johnson4110@gmail.com>
2016-10-18 11:14:12 -0400

    styled page template and added responsiveness

--- wp-content/themes/war-2016/template-parts/content.php
+++ wp-content/themes/war-2016/template-parts/content.php
@@ -17,9 +17,7 @@
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 	</header><!-- .entry-header -->
 
-	<?php twentysixteen_excerpt(); ?>
-
-	<?php twentysixteen_post_thumbnail(); ?>
+	
 
 	<div class="entry-content">
 		<?php
