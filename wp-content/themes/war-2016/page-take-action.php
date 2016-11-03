bf91c9e1432c7f7352e09e60bf7331be45009610
matt4110 <matthew.johnson4110@gmail.com>
2016-11-03 11:36:19 -0400

    take action page

--- wp-content/themes/war-2016/page-take-action.php
+++ wp-content/themes/war-2016/page-take-action.php
@@ -11,38 +11,50 @@ get_header(); ?>
 
 			<!-- 1st column -->
 			<div class="column-1-3">
+				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Time-with-framing.png">
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Host a Party</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/host-a-party">Host a Party</a></h1>
+					<p class="small-text">Promote justice by hosting a WAR conference or party. Increasing awareness and product sales is essential to empowering the rescued and at-risk!</p>
 				</div>
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Volunteer</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/take-action/volunteer">Volunteer</a></h1>
+					<p class="small-text">Join the fight for freedom as an HQ volunteer or represent WAR at events in your area. Large or small groups are welcome!</p>
 				</div>
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Join Our Team</a></h1 class="column-heading" >
-				</div>
-			</div>
-
-			<div class="column-1-3">
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Join our Professional Circle</a></h1 class="column-heading" >
-				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Share your creative skills</a></h1 class="column-heading" >
-				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Become a Circle of Protection</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/take-action/join-our-team/">Join Our Team</a></h1>
+					<p class="small-text">Become a voice for the silenced on WAR's style committee, as a product consultant, or even through unpaid internships at HQ</p>
 				</div>
 			</div>
 
 			<div class="column-1-3">
+				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Talent-with-framing.png">
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Items Old & New</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Join our Professional Circle</a></h1>
+					<p class="small-text">Support WAR's programs and partners by advising our leaders. All people and professions are welcome!</p>
 				</div>
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Through Your Purchases</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Share your creative skills</a></h1>
+					<p class="small-text">With your writing, photography, graphic arts, or unique talents, take action against injustice! Empower women through beauty & art!</p>
 				</div>
 				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give your sweat, space, or fundraising solutions</a></h1 class="column-heading" >
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Become a Circle of Protection</a></h1>
+					<p class="small-text">We need you to become a circle of protection! Learn to recognize the signs of trafficking in your home town or even mentor at-risk and rescued women!</p>
+				</div>
+			</div>
+
+			<div class="column-1-3">
+				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Treasure-with-framing.png">
+				<div class="column-box">
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Items Old & New</a></h1>
+					<p class="small-text">Diapers, clothes, & crayons have the power to change a life! Learn how you can offer hope & healing!</p>
+				</div>
+				<div class="column-box">
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Through Your Purchases</a></h1>
+					<p class="small-text">Let women know they are valuable! Buy & brag on their beautiful products, empowering them to work with purpose!</p>
+				</div>
+				<div class="column-box">
+					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give your sweat, space, or fundraising solutions</a></h1>
+					<p class="small-text">Support the at-risk in a way that works for you! Sell product, join our racing team, or invent your own solution!</p>
 				</div>
 			</div>
 			<div class="clearfix">
