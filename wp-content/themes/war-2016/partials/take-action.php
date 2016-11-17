4f21f073df37319558360f54dfcf8b01b811919b
matt4110 <matthew.johnson4110@gmail.com>
2016-11-17 09:17:32 -0500

    call to action module

--- wp-content/themes/war-2016/partials/take-action.php
+++ wp-content/themes/war-2016/partials/take-action.php
@@ -4,23 +4,23 @@
 */
 //include bootstrap
 //get_template_part( 'partials/bootstrap' );?>
-<div class="filigree">&nbsp;</div>
-<div class="red-stripe-box">
-	<div class="row">
+<div class="filigree" style="margin-top:75px;">&nbsp;</div>
+<div class="red-stripe-box" style="margin-bottom: 75px;">
+	<!--<div class="row">
 		<h1 class="cta-header">Take Action</h1>
-	</div>
+	</div>-->
 	<div class="row">
-			<div class="grid-1-3">
+			<div class="cta-grid">
 				<a href="http://warinternational.org/donate"><img src="https://warinternational.org/wp-content/uploads/2016/09/take-action-donate.png"></a>
 				</br>
 				<a class="cta-link" href="http://warinternational.org/donate">Donate</a>
 			</div>
-			<div class="grid-1-3">
+			<div class="cta-grid">
 				<a href="http://warinternational.org/get-involved"><img src="https://warinternational.org/wp-content/uploads/2016/09/take-action-volunteer.png"></a>
 				</br>
 				<a class="cta-link" href="http://warinternational.org/get-involved">Volunteer</a>
 			</div>
-			<div class="grid-1-3" >
+			<div class="cta-grid" >
 				<a href="http://warchestboutique.com/"><img src="https://warinternational.org/wp-content/uploads/2016/09/take-action-shop.png"></a>
 				</br>
 				<a class="cta-link" href="http://warchestboutique.com/">Shop</a>
