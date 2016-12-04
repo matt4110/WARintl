8aae5a77c17e643730a97c3c389063a44ce49a31
matt4110 <matthew.johnson4110@gmail.com>
2016-12-03 23:46:39 -0500

    img border

--- wp-content/themes/war-2016/page-upcoming-events.php
+++ wp-content/themes/war-2016/page-upcoming-events.php
@@ -11,8 +11,8 @@ get_header(); ?>
 			<div class="standard-content">
 				<div class="row">
 					<div class="grid-1-3">
-					    <a href=<?php get_bloginfo('url'); ?>"/cfr/"><h2>CFR</h2>
-					    <img class="square-img" src=<?php get_bloginfo('url'); ?>"/images/cfr/cfr-grfirst.jpg"></a>
+					    <a href=<?php get_bloginfo('url'); ?>"/programs/civilian-first-responder-cfr/"><h2>CFR</h2>
+					    <img class="square-img border" src=<?php get_bloginfo('url'); ?>"/images/cfr/cfr-grfirst.jpg"></a>
 					    <p>Jan 13-14, 2017</p>
 					    <p>GR First Assembly<br>2100 44th St<br>Wyoming, MI 49519</p>
 					</div>
@@ -20,7 +20,7 @@ get_header(); ?>
 
 					<div class="grid-1-3">
 					    <a href=<?php get_bloginfo('url'); ?>"/teamwar"><h2>Gazelle Girl</h2>
-					    <img class="square-img" src=<?php get_bloginfo('url'); ?>"/images/teamwar-square.jpg"></a>
+					    <img class="square-img border" src=<?php get_bloginfo('url'); ?>"/images/teamwar-square.jpg"></a>
 					    <p>Feb 28, 2017</p>
 					    <p>Calder Plaza<br>Grand Rapids, MI 49503</p>
 					</div>
@@ -28,7 +28,7 @@ get_header(); ?>
 
 					<div class="grid-1-3">
 					    <a href=<?php get_bloginfo('url'); ?>"/circle-tour/"><h2>Circle Tour</h2>
-					    <img class="square-img" src=""></a>
+					    <img class="square-img border" src=""></a>
 					    <p>February 2017</p>
 					    <p>Thailand</p>
 					</div>
