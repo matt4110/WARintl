dc7ed1f626b0ed81112ca00bd0ff2c0910090ae5
matt4110 <matthew.johnson4110@gmail.com>
2016-12-27 08:24:53 -0500

    restructured take action pages with flexbox

--- wp-content/themes/war-2016/page-take-action.php
+++ wp-content/themes/war-2016/page-take-action.php
@@ -10,54 +10,58 @@ get_header(); ?>
 			<div class="standard-content">
 
 			<!-- 1st column -->
-			<div class="column-1-3">
-				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Time-with-framing.png">
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/host-a-party">Host a Party</a></h1>
+			<div class="column-grid">
+				<img class="column-img one-12" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Time-with-framing.png">
+
+				<div class="column-box four-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/host-a-party">Host a Party</a></h1>
 					<p class="small-text">Promote justice by hosting a WAR conference or party. Increasing awareness and product sales is essential to empowering the rescued and at-risk!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/take-action/volunteer">Volunteer</a></h1>
+
+				<div class="column-box seven-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/volunteer">Volunteer</a></h1>
 					<p class="small-text">Join the fight for freedom as an HQ volunteer or represent WAR at events in your area. Large or small groups are welcome!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"/take-action/join-our-team/">Join Our Team</a></h1>
+
+				<div class="column-box ten-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/join-our-team/">Join Our Team</a></h1>
 					<p class="small-text">Become a voice for the silenced on WAR's style committee, as a product consultant, or even through unpaid internships at HQ</p>
 				</div>
-			</div>
+			
+				<img class="column-img two-12" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Talent-with-framing.png">
 
-			<div class="column-1-3">
-				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Talent-with-framing.png">
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Join our Professional Circle</a></h1>
+				<div class="column-box five-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/join-our-professional-circle">Join our Professional Circle</a></h1>
 					<p class="small-text">Support WAR's programs and partners by advising our leaders. All people and professions are welcome!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Share your creative skills</a></h1>
+
+				<div class="column-box eight-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/share-your-creative-skills">Share your creative skills</a></h1>
 					<p class="small-text">With your writing, photography, graphic arts, or unique talents, take action against injustice! Empower women through beauty & art!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Become a Circle of Protection</a></h1>
+
+				<div class="column-box eleven-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/become-a-circle-of-protection">Become a Circle of Protection</a></h1>
 					<p class="small-text">We need you to become a circle of protection! Learn to recognize the signs of trafficking in your home town or even mentor at-risk and rescued women!</p>
 				</div>
-			</div>
+			
+				<img class="column-img three-12" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Treasure-with-framing.png">
 
-			<div class="column-1-3">
-				<img class="column-img" src=<?php get_bloginfo('url'); ?>"/wp-content/uploads/2015/01/Treasure-with-framing.png">
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Items Old & New</a></h1>
+				<div class="column-box six-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/donations">Give Items Old & New</a></h1>
 					<p class="small-text">Diapers, clothes, & crayons have the power to change a life! Learn how you can offer hope & healing!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give Through Your Purchases</a></h1>
+
+				<div class="column-box nine-12">
+					<h1 class="column-heading centered" ><a href="http://warchestboutique.com">Give Through Your Purchases</a></h1>
 					<p class="small-text">Let women know they are valuable! Buy & brag on their beautiful products, empowering them to work with purpose!</p>
 				</div>
-				<div class="column-box">
-					<h1 class="column-heading" ><a href=<?php get_bloginfo('url'); ?>"">Give your sweat, space, or fundraising solutions</a></h1>
+
+				<div class="column-box twelve-12">
+					<h1 class="column-heading centered" ><a href=<?php get_bloginfo('url'); ?>"/fundraising">Give your sweat, space, or fundraising solutions</a></h1>
 					<p class="small-text">Support the at-risk in a way that works for you! Sell product, join our racing team, or invent your own solution!</p>
 				</div>
 			</div>
-			<div class="clearfix">
 
 			</div><!-- .standard-content --> 
 		</main><!-- .site-main -->
---
+++ wp-content/themes/war-2016/page-talent.php
@@ -0,0 +1,45 @@
+<?php
+/*
+ Template Name: Talent-pages
+*/
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="standard-content">
+
+			<?php if ( have_posts() ) : ?>
+
+					<header class="ta-header">
+						<h1 class="post-title"><?php single_post_title(); ?></h1>
+						<img src="http://warinternational.org/wp-content/uploads/2015/01/Talent-with-framing.png">
+					</header>
+					<div style="margin-bottom:25px;" class="clearfix">&nbsp;</div>
+
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'template-parts/content', 'notitle' );
+
+				// End the loop.
+				endwhile;
+
+			// If no content, include the "No posts found" template.
+			else :
+				get_template_part( 'template-parts/content', 'none' );
+
+			endif;
+			?>
+
+			</div><!-- .standard-content -->
+		</main><!-- .site-main -->
+	</div><!-- .content-area -->
+
+<?php get_footer(); ?>
