f3700ddde56b148e1a78b13c3d33941a305e4f21
matt4110 <matthew.johnson4110@gmail.com>
2017-01-14 22:17:32 -0500

    wording

--- wp-content/themes/war-2016/page-donate.php
+++ wp-content/themes/war-2016/page-donate.php
@@ -21,7 +21,7 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/international-safehouses">
 				    <h3 class="centered">International<br>Safehouses</h3></a>
 				    <hr>
-				    <p class="small-white-text">Creating circles of protection, for men, women, and children to find refuge.</p>
+				    <p class="small-white-text">Creating circles of protection for men, women, and children to find refuge.</p>
 				</div>
 
 
@@ -29,7 +29,7 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/domestic-safehouses">
 				    <h3 class="centered">Domestic<br>Safehouses</h3></a>
 				    <hr>
-				    <p class="small-white-text">This normally looks like outreach to help women and children out of trafficking into a life of dignity</p>
+				    <p class="small-white-text">Helping women and children out of trafficking into lives of dignity.</p>
 				</div>
 
 
@@ -37,14 +37,14 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/vocational-training">
 				    <h3 class="centered">Vocational<br>Training</h3></a>
 				    <hr>
-				    <p class="small-white-text">Equipping people so they have the economic freedom to break the chains of bondage</p>
+				    <p class="small-white-text">Equipping at-risk men and women with the skills to gain economic freedom.</p>
 				</div>
 			
 				<div class="red-box2">
 				    <a href=<?php get_bloginfo('url'); ?>"/micro-enterprises">
 				    <h3 class="centered">Micro<br>Enterprises</h3></a>
 				    <hr>
-				    <p class="small-white-text">Investing in peoples future, giving them a leg up to live a life of dignity</p>
+				    <p class="small-white-text">Investing in lives by giving those at risk a “hand up” toward sustainability.</p>
 				</div>
 
 
@@ -52,7 +52,7 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/education">
 				    <h3 class="centered">Education</h3></a>
 				    <hr>
-				    <p class="small-white-text">Knowledge is power, and we want to empower people to give them a life of dignity</p>
+				    <p class="small-white-text">Empowering people through knowledge to give them hope for a better future.</p>
 				</div>
 
 
@@ -60,14 +60,14 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/orphanages">
 				    <h3 class="centered">Orphanages</h3></a>
 				    <hr>
-				    <p class="small-white-text">True religion is taking care of the widows and orphans</p>
+				    <p class="small-white-text">Embodying true religion by caring for “the least of these.”</p>
 				</div>
 			
 				<div class="red-box2">
 				    <a href=<?php get_bloginfo('url'); ?>"/outreach">
 				    <h3 class="centered">Outreach</h3></a>
 				    <hr>
-				    <p class="small-white-text">Walking the red light districts to meet victims where there are, to help them out of a life of darkness</p>
+				    <p class="small-white-text">Walking red-light districts to meet victims where they are and help them out of lives of darkness.</p>
 				</div>
 
 
@@ -75,7 +75,7 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/medical-aid">
 				    <h3 class="centered">Medical Aid</h3></a>
 				    <hr>
-				    <p class="small-white-text">A healthy body and mind are integral parts of healing and prevention</p>
+				    <p class="small-white-text">Promoting healthy bodies and minds as integral aspects of healing and prevention.</p>
 				</div>
 
 
@@ -83,7 +83,7 @@ get_header(); ?>
 				    <a href=<?php get_bloginfo('url'); ?>"/emergency-intervention">
 				    <h3 class="centered">Emergency<br>Intervention</h3></a>
 				    <hr>
-				    <p class="small-white-text">A quick reaction to help prevent someone from becoming a victim</p>
+				    <p class="small-white-text">Acting on short notice when there is an immediate need.</p>
 				</div>
 			</div>
 
