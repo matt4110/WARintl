fc62cc2317682d81f43269089bab6ee824da05e8
matt4110 <matthew.johnson4110@gmail.com>
2017-02-28 09:57:28 -0500

    started changin location and hours page

---
+++ wp-content/themes/war-2016/page-locations-hours.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ Template Name: locations-hours
+
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<div class="standard-content">
+				
+				<!--Headquarters-->
+				<h2 style="text-align: center;">WAR, Int'l Headquarters</h2>
+				<div>
+					
+				</div>
+
+				&nbsp;
+				<hr>
+				&nbsp;
+
+				<!--Warchest Boutiques-->
+				<h2 style="text-align: center;">WARChest Boutique Stores</h2>
+				<div class="row">
+					<!--Wyoming WARChest Boutique-->
+					<div class="grid-1-3">
+						<p><strong>Wyoming, MI</strong><br>
+						(616)530-1234</p>
+						<img src="http://warinternational.org/wp-content/uploads/2012/04/storesign.jpg">
+						<p>2790 44th St SW<br>
+						Wyoming, MI 49519</p>
+						<p>Hours*:<br>
+						Mon-Sat: 10am-6pm<br>
+						Closed Sunday<br>
+						</p>
+					</div>
+
+					<!--Rockford WARChest Boutique-->
+					<div class="grid-1-3">
+						<p><strong>Rockford, MI</strong><br>
+						(616)863-0100</p>
+						<img src="http://warinternational.org/wp-content/uploads/2012/04/Rockford.jpg">
+						<p style="letter-spacing: .03rem;">25 Squires ST Square NE<br>
+						Rockford, MI 49341</p>
+						<p>Hours*:<br>
+						<span style="letter-spacing: .03rem;">Mon-Fri: 10:30am-7pm</span><br>
+						Saturday: 10am-6pm<br>
+						Sunday: 12pm-5pm</p>
+					</div>
+
+					<!--Naperville WARChest Boutique-->
+					<div class="grid-1-3">
+						<p><strong>Naperville, IL</strong><br>
+						(630)904-9200</p>
+						<img src="http://warinternational.org/wp-content/uploads/2014/03/warchestboutique-naperville-300x190.jpg">
+						<p>4 East Jefferson AVE<br>
+						Naperville, IL 60540</p>
+						<p>Hours*:<br>
+						Closed Monday<br>
+						<span style="letter-spacing: .03rem;">Tues-Sat: 11am-6:30pm</span><br>
+						Sunday: 12pm-4pm</p>
+					</div>
+
+				</div>
+				&nbsp;
+				<hr>
+				&nbsp;
+				<!--Tea Trade Cafe-->
+				<h2 style="text-align: center;">Tea Trade Cafe</h2>
+
+
+		
+			</div>
+		</main>
+	</div><!-- .content-area -->
+
+
+<?php get_footer(); ?>
