95821ee6fe13684be688b4c684a5580f71a0643b
matt4110 <matthew.johnson4110@gmail.com>
2018-04-17 23:04:56 -0400

    cfr changes

--- wp-content/themes/war-2016/page-cfr.php
+++ wp-content/themes/war-2016/page-cfr.php
@@ -3,7 +3,6 @@
    Template Name: CFR
  * The template for displaying the Civilian First Responder Page
  */
-
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
@@ -30,15 +29,16 @@ get_header(); ?>
 			<div class="standard-content">
 			<!-- TEXT div -->
 			<div>
-				<p class="small-p" style="text-align: justify;">No longer do we have the luxury of deciding if whether or not we need to know about human trafficking. Learn how to become a circle of protection around the loved ones in your life and come to one of our Civilian First Responder (CFR) conferences. Here, you will learn what makes a community susceptible to trafficking, how to identify a victim, how to respond to and report suspicious activity, and more. Discover helpful tips, action plans, and ways to protect the innocent.</p>
+				<p class="small-p" style="text-align: justify;">No longer do we have the luxury of deciding whether or not we need to know about human trafficking. Learn how to become a circle of protection around the loved ones in your life by coming to one of our Civilian First Responder (CFR) conferences. Here, you will learn what makes a community susceptible to trafficking, how to identify a victim, how to respond to and report suspicious activity, and more. Discover helpful tips, action plans, and ways to protect the innocent.</p>
 				<p class="small-p" style="text-align: justify;">This highly sought-after conference brings the U.S. government, Homeland Security, law enforcement officers, human trafficking survivors, and the president of Women At Risk, International into one room to educate communities on this dangerous crime. </p>
 
 				<ul class="small-p" style="list-style: disc;"><b>Three Good Reasons to Host or Attend a CFR Event</b>
-					<li>Shows you the signs of trafficking in your community</li>
-					<li>Healthy ways to respond to crisis situations</li>
-					<li>How to share this information with your community members, no matter their age</li>
+					<li>You will learn to recognize the signs of trafficking in your community</li>
+					<li>You will learn healthy ways to respond to crisis situations</li>
+					<li>You will learn how to share this information with community members, of any age</li>
 				</ul>
 			</div>
+			<span id="upcoming-cfrs"></span>
 			&nbsp;
 
 			<!-- *********************
@@ -54,51 +54,29 @@ get_header(); ?>
 
 
 				<div class="cfr-item">
-					<a href="https://warinternational.org/cfr-registration/?event_name=rosemount">
+					<a href="https://warinternational.org/cfr-registration/?event_name=holland">
 					<div class="cfr-item-date">
-						<p class="cfr-item-month">Aug</p>
-						<p class="cfr-item-day">12</p>
+						<p class="cfr-item-month">Apr</p>
+						<p class="cfr-item-day">21</p>
 					</div>
-					<p class="cfr-item-location">Lighthouse<br>Christian Church</p>
-					<p class="cfr-item-sub-location">Rosemount, MN<br>
-					<a href="http://warinternational.org/images/cfr/8-12-17.pdf" data-lity>(more info)</a></p>
-					</a>
+					<p class="cfr-item-location">Ridge Point<br>Community Church</p>
+					<p class="cfr-item-sub-location">Holland, MI<br>
+					<p class="cfr-item-sub-location">9am - 4:30pm<br>
+					<a href=<?php get_bloginfo( 'url' ); ?>"/2018-cfr-holland/" target="_blank">(more info)</a></p></a>
 				</div>
 
 				<div class="cfr-item">
-					<a href="https://warinternational.org/cfr-registration/?event_name=catharine">
+					<a href="https://warinternational.org/cfr-registration/?event_name=alpena">
 					<div class="cfr-item-date">
-						<p class="cfr-item-month">sep</p>
+						<p class="cfr-item-month">Jun</p>
 						<p class="cfr-item-day">16</p>
 					</div>
-					<p class="cfr-item-location">Saint Catharine<br>School</p>
-					<p class="cfr-item-sub-location">Spring Lake, NJ<br>
-					<a href="http://warinternational.org/images/cfr/9-16-17.pdf" data-lity>(more info)</a></p>
-					</a>
+					<p class="cfr-item-location">New Life<br>Christian Fellowship</p>
+					<p class="cfr-item-sub-location">Alpena, MI<br>
+					<p class="cfr-item-sub-location">9am - 4:30pm<br>
+					<a href=<?php get_bloginfo( 'url' ); ?>"/2018-cfr-alpena/" target="_blank">(more info)</a></p></a>
 				</div>
 
-				<div class="cfr-item">
-					<a href="https://warinternational.org/cfr-registration/?event_name=port-huron">
-					<div class="cfr-item-date">
-						<p class="cfr-item-month">Oct</p>
-						<p class="cfr-item-day">07</p>
-					</div>
-					<p class="cfr-item-location">Colonial Woods<br>Missionary Church</p>
-					<p class="cfr-item-sub-location">Port Huron, MI<br>
-					<a href="http://warinternational.org/images/cfr/10-7-17.pdf" data-lity>(more info)</a></p></a>
-				</div>
-
-				<div class="cfr-item">
-					<a href="https://warinternational.org/cfr-registration/?event_name=witchita">
-					<div class="cfr-item-date">
-						<p class="cfr-item-month">nov</p>
-						<p class="cfr-item-day">04</p>
-					</div>
-					<p class="cfr-item-location">Global Portal<br>4 Freedom<br>Hotel At Old Town</p>
-					<p class="cfr-item-sub-location">Witchita, KS<br>
-					<a href="">(more info to come)</a></p>
-					</a>
-				</div>
 
 			</div>
 
@@ -108,8 +86,7 @@ get_header(); ?>
 
 				<p style="text-align: center;">For information on how to bring a CFR conference to your area, email us today at <a class="text-link" href="MAILTO:info@warinternational.org">info@warinternational.org</a></p>
 				<hr>
-
-
+			
 			<!-- *********************
 			**************************
 			Continuing Ed
@@ -121,22 +98,20 @@ get_header(); ?>
 			<p class="small-text" style="text-align: center;">(only in Michigan)</p>
 			<p style="text-align: justify;">If you are interested in receiving continuing education credits for the event(s) below, preregistration is required. One credit hour equals one continuing education credit for social workers. If you have any additional questions about how to obtain credit for attending the course(s) below, please email our staff at <a href="MAILTO:info@warinternational.org">info@warinternational.org</a></p>
 
-
 			<div id="upcoming-cfr">
 
-			<p>Thank you for your interest in our CE opportunities. We currently do not have any upcoming events. Please continue to check this page for updates on events in your area.</p>
-<!--
 				<div class="cfr-item">
-					<a href="https://warinternational.org/ce-registration">
+					<a href=" https://churchteams.com/m/Login.asp?oID=13098&secID=a2RmRmhkdkZFV0JpVlkwa21kQlFLZHk0anJsTzQ1RGo%3D&page=Register.asp?GroupID=331753~sgr=y ">
 					<div class="cfr-item-date">
-						<p class="cfr-item-month">May</p>
-						<p class="cfr-item-day">05</p>
+						<p class="cfr-item-month">Jun</p>
+						<p class="cfr-item-day">15</p>
 					</div>	
-					<p class="cfr-item-location">Oak Pointe Church<p>
-					<p class="cfr-item-sub-location">Novi, MI</p></a>
+					<p class="cfr-item-location">New Life Christian Fellowship<p>
+					<p class="cfr-item-sub-location">Alpena, MI</p></a>
+					<a href="#CEU-P" class="cfr-item-sub-location" data-lity>(more info)</a>
 				</div>
--->
 			</div>
+			<p id="CEU-P" class="lity-hide" style="padding:30px;">This seminar, for medical professionals, mental health/substance use disorder treatment therapists, case managers, and others in healthcare/social service settings, will shed light on a subject so dark that many do not know it exists. Through case histories and statistics, those in helping professions will be equipped to heal brokenness by being able to identify notable indicators of 5 categories of health concerns and symptoms, identify 3 sets of players in trafficking scenarios in the healthcare environment, and to develop an appropriate assistance plan for victims. *CEU credits are only available for social workers and substance abuse professionals.</p>
 
 
 			<!-- *********************
@@ -153,12 +128,10 @@ get_header(); ?>
 					<p class="small-p" style="letter-spacing: .04rem;">Teach the middle schoolers, high schoolers, and college students in your life about the dangers of human trafficking
 					<a href="MAILTO:youth@warinternational.org">youth@warinternational.org</a></p>
 				</div>
-
 				<div class="grid-1-3">
 					<div class="cta-title-bar">Industry Specific</div>
 					<p class="small-p" style="letter-spacing: .04rem;">Tailored to individuals and professional groups, including medical professionals, military personnel, hospitality workers, and more.</p>
 				</div>
-
 				<div class="grid-1-3">
 					<div class="cta-title-bar">Resources</div>
 					<ul class="small-p" style="padding-left: 0;">
@@ -167,7 +140,6 @@ get_header(); ?>
 						<li><a class="text-link" href="https://www.state.gov/documents/organization/258876.pdf" target="_blank">Trafficking in Persons (TIP) Report</a></li>
 					</ul>
 				</div>
-
 			</div>-->
 			
 
